bb5d669939901337a67f262e3777fdd5e78b3090
Nguyen Minh Trung <nguyenminhtrung18072004@gmail.com>
2025-09-29 15:55:35 +0700

    refactor(community): move classes into new folder structure

---
+++ Pento/src/Modules/Community/Pento.Modules.Community.Application/BlogPosts/Create/CreateBlogPostCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Pento.Modules.Community.Application.BlogPosts.Create;
+internal sealed class CreateBlogPostCommandValidator : AbstractValidator<CreateBlogPostCommand>
+{
+    public CreateBlogPostCommandValidator()
+    {
+
+    }
+}
---
+++ Pento/src/Modules/Community/Pento.Modules.Community.Application/GiveawayClaims/Get/GiveawayClaimResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pento.Modules.Community.Application.GiveawayClaims.Get;
+public sealed record GiveawayClaimResponse(Guid ClaimId)
+{
+}
