4e0e01c0c94ef4f636ca9a7bd7fec94210514bad
Nguyen Minh Trung <nguyenminhtrung18072004@gmail.com>
2025-11-18 09:15:10 +0700

    add GetMealPlanReservationsByHouseholdId

---
+++ Pento/src/Pento.API/Endpoints/MealPlans/Get/GetMealPlanReservations.cs
@@ -0,0 +1,30 @@
+﻿using Pento.API.Extensions;
+using Pento.Application.Abstractions.Messaging;
+using Pento.Application.FoodItemReservations.Get;
+using Pento.Application.MealPlans.Reserve;
+using Pento.Domain.Abstractions;
+
+namespace Pento.API.Endpoints.MealPlans.Get;
+
+internal sealed class GetMealPlanReservations : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("meal-plan-reservations/current-household", async (
+            IQueryHandler<GetMealPlanReservationsByHouseholdIdQuery, IReadOnlyList<MealPlanReservationResponse>> handler,
+            CancellationToken cancellationToken
+        ) =>
+        {
+            var query = new GetMealPlanReservationsByHouseholdIdQuery();
+
+            Result<IReadOnlyList<MealPlanReservationResponse>> result = await handler.Handle(query, cancellationToken);
+
+            return result.Match(
+                Results.Ok,
+                CustomResults.Problem
+            );
+        })
+        .WithTags(Tags.MealPlans)
+        .RequireAuthorization();
+    }
+}
---
+++ Pento/src/Pento.Application/MealPlans/Reserve/MealPlanReservationResponse.cs
@@ -0,0 +1,13 @@
+﻿using Pento.Domain.FoodItemReservations;
+
+namespace Pento.Application.MealPlans.Reserve;
+
+public sealed record MealPlanReservationResponse(
+    Guid Id,
+    Guid FoodItemId,
+    Guid MealPlanId,
+    decimal Quantity,
+    Guid UnitId,
+    DateTime ReservationDateUtc,
+    ReservationStatus Status
+);
