1433026c489cd34e0edc4959d3d9e021df880487
NET13_NguyenPhuocDuy <duynguyen4100@gmail.com>
2025-11-28 18:07:39 +0700

    new migration

---
+++ Pento/src/Pento.Infrastructure/Migrations/20251128110710_UserEntitlementNew.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pento.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class UserEntitlementNew : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "is_archived",
+            table: "user_entitlements");
+
+        migrationBuilder.DropColumn(
+            name: "is_deleted",
+            table: "user_entitlements");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<bool>(
+            name: "is_archived",
+            table: "user_entitlements",
+            type: "boolean",
+            nullable: false,
+            defaultValue: false);
+
+        migrationBuilder.AddColumn<bool>(
+            name: "is_deleted",
+            table: "user_entitlements",
+            type: "boolean",
+            nullable: false,
+            defaultValue: false);
+    }
+}
