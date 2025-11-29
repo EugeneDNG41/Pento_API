d81425c21a792f04a34ecf0a9c6538d4d2ade6c5
Nguyen Minh Trung <nguyenminhtrung18072004@gmail.com>
2025-11-30 02:51:03 +0700

    GET claims

---
+++ Pento/src/Pento.API/Endpoints/GiveawayClaims/Get/SearchGiveawayClaimsEndpoint.cs
@@ -0,0 +1,62 @@
+﻿using Pento.API.Extensions;
+using Pento.Application.Abstractions.Messaging;
+using Pento.Application.Abstractions.Pagination;
+using Pento.Application.GiveawayClaims.Get;
+using Pento.Application.GiveawayClaims.Search;
+using Pento.Application.Giveaways.Claims.Get;
+using Pento.Application.Giveaways.Claims.Search;
+using Pento.Domain.GiveawayClaims;
+
+namespace Pento.API.Endpoints.Giveaways.Claims;
+
+internal sealed class SearchGiveawayClaimsEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("giveawayclaims/search", async (
+            Guid? giveawayPostId,
+            ClaimStatus? status,
+            string? searchText,
+            string? sortBy,  
+            bool descending,
+            int pageNumber,
+            int pageSize,
+            IQueryHandler<SearchGiveawayClaimQuery, PagedList<GiveawayClaimPreview>> handler,
+            CancellationToken ct
+        ) =>
+        {
+            var query = new SearchGiveawayClaimQuery(
+                giveawayPostId,
+                status,
+                searchText,
+                sortBy,
+                descending,
+                pageNumber,
+                pageSize
+            );
+
+            Domain.Abstractions.Result<PagedList<GiveawayClaimPreview>> result = await handler.Handle(query, ct);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .WithTags(Tags.GiveawayClaims)
+        .WithDescription("sort by: post, date")
+        .RequireAuthorization();
+
+        app.MapGet("giveawayclaims/{id:guid}", async (
+            Guid id,
+            IQueryHandler<GetGiveawayClaimQuery, GiveawayClaimDetailResponse> handler,
+            CancellationToken ct
+        ) =>
+        {
+            Domain.Abstractions.Result<GiveawayClaimDetailResponse> result = await handler.Handle(new GetGiveawayClaimQuery(id), ct);
+
+            return result.Match(
+                Results.Ok,
+                CustomResults.Problem
+            );
+        })
+        .WithTags(Tags.GiveawayClaims)
+        .RequireAuthorization();
+    }
+}
---
+++ Pento/src/Pento.Application/GiveawayClaims/Search/SearchGiveawayClaimQuery.cs
@@ -0,0 +1,17 @@
+﻿using Pento.Application.Abstractions.Messaging;
+using Pento.Application.Abstractions.Pagination;
+using Pento.Application.GiveawayClaims.Search;
+using Pento.Domain.GiveawayClaims;
+
+namespace Pento.Application.Giveaways.Claims.Search;
+
+public sealed record SearchGiveawayClaimQuery(
+    Guid? GiveawayPostId,
+    ClaimStatus? Status,
+    string? SearchText,
+    string? SortBy,         
+    bool Descending,         
+    int PageNumber,
+    int PageSize
+) : IQuery<PagedList<GiveawayClaimPreview>>;
+
