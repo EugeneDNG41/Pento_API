de70a18fff5e7805d40f7058fed1609941d866b2
Nguyen Minh Trung <nguyenminhtrung18072004@gmail.com>
2025-11-30 05:00:50 +0700

    GET all reservation for donation with sort and pagination

---
+++ Pento/src/Pento.API/Endpoints/GiveawayPosts/Get/Reservation/GetDonationReservationsEndpoint.cs
@@ -0,0 +1,41 @@
+﻿using Pento.API.Endpoints;
+using Pento.API.Extensions;
+using Pento.Application.Abstractions.Messaging;
+using Pento.Application.Abstractions.Pagination;
+using Pento.Application.GiveawayPosts.Get.Reservation;
+using Pento.Domain.FoodItemReservations;
+
+namespace Pento.API.Endpoints.GiveawayPosts.Get.Reservation;
+internal sealed class GetDonationReservationsEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("donation-reservations", async (
+            Guid? giveawayPostId,
+            Guid? foodReferenceId,
+            ReservationStatus? status,
+            int pageNumber,
+            int pageSize,
+            IQueryHandler<GetDonationReservationsQuery, PagedList<DonationReservationResponse>> handler,
+            CancellationToken ct
+        ) =>
+        {
+            pageNumber = pageNumber <= 0 ? 1 : pageNumber;
+            pageSize = pageSize <= 0 ? 10 : pageSize;
+
+            var query = new GetDonationReservationsQuery(
+                GiveawayPostId: giveawayPostId,
+                FoodReferenceId: foodReferenceId,
+                Status: status,
+                PageNumber: pageNumber,
+                PageSize: pageSize
+            );
+
+            Pento.Domain.Abstractions.Result<PagedList<DonationReservationResponse>> result = await handler.Handle(query, ct);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .WithTags(Tags.Reservations)
+        .RequireAuthorization();
+    }
+}
---
+++ Pento/src/Pento.Application/GiveawayPosts/Get/Reservation/DonationReservationResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pento.Domain.FoodItemReservations;
+
+namespace Pento.Application.GiveawayPosts.Get.Reservation;
+public sealed record DonationReservationResponse(
+    Guid Id,
+    Guid GiveawayPostId,
+    string TitleDescription,
+    string ContactInfo,
+    string Address,
+
+    Guid FoodItemId,
+    Guid FoodReferenceId,
+    string FoodReferenceName,
+    Uri? FoodReferenceImageUrl,
+    string FoodGroup,
+
+    decimal Quantity,
+    string UnitAbbreviation,
+
+    DateTime ReservationDateUtc,
+    ReservationStatus Status
+);
+
