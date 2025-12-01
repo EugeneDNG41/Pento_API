84ad9de373cf77af84dc57ea8732431dbf80c0c5
NET13_NguyenPhuocDuy <duynguyen4100@gmail.com>
2025-12-01 17:29:01 +0700

    add get milestones for current user

---
+++ Pento/src/Pento.Application/UserMilestones/GetCurrentMilestones/GetCurrentMilestonesQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Pento.Application.UserMilestones.GetCurrentMilestones;
+
+internal sealed class GetCurrentMilestonesQueryValidator : AbstractValidator<GetCurrentMilestonesQuery>
+{
+    public GetCurrentMilestonesQueryValidator()
+    {
+        RuleFor(x => x.PageNumber)
+            .GreaterThan(0).WithMessage("Page number must be greater than 0.");
+        RuleFor(x => x.PageSize)
+            .GreaterThan(0).WithMessage("Page size must be greater than 0.");
+    }
+}
