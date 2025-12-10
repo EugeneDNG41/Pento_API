3201fe5e798cec63a0bd47b099404f799f940d54
Nguyen Minh Trung <nguyenminhtrung18072004@gmail.com>
2025-12-10 17:08:44 +0700

    get all notifications of user

---
+++ Pento/src/Pento.Application/Notifications/Get/GetNotificationsQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using Pento.Application.Abstractions.Authentication;
+using Pento.Application.Abstractions.Messaging;
+using Pento.Application.Abstractions.Persistence;
+using Pento.Domain.Abstractions;
+using Pento.Domain.Notifications;
+
+namespace Pento.Application.Notifications.Get;
+
+internal sealed class GetNotificationsQueryHandler(
+    IUserContext userContext,
+    IGenericRepository<Notification> notificationRepo
+) : IQueryHandler<GetNotificationsQuery, List<NotificationResponse>>
+{
+    public async Task<Result<List<NotificationResponse>>> Handle(
+        GetNotificationsQuery request,
+        CancellationToken cancellationToken)
+    {
+        Guid userId = userContext.UserId;
+
+        IEnumerable<Notification> notifications = await notificationRepo.FindAsync(
+            x => x.UserId == userId,
+            cancellationToken
+        );
+
+        var list = notifications
+            .OrderByDescending(n => n.SentOn)
+            .Select(n => new NotificationResponse(
+                n.Id,
+                n.Title,
+                n.Body,
+                n.Type.ToString(),
+                n.DataJson,
+                n.SentOn,
+                n.ReadOn
+            ))
+            .ToList();
+
+        return Result.Success(list);
+    }
+}
