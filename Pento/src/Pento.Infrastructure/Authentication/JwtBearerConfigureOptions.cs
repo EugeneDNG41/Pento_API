265a6e6337be7bd5bb63e32a33f5ec6cebaf6df9
Nguyen Minh Trung <nguyenminhtrung18072004@gmail.com>
2025-12-22 22:20:26 +0700

    add signalr to jwt config

--- Pento/src/Pento.Infrastructure/Authentication/JwtBearerConfigureOptions.cs
+++ Pento/src/Pento.Infrastructure/Authentication/JwtBearerConfigureOptions.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
-
+using Microsoft.Extensions.Primitives;
+using Microsoft.AspNetCore.Http;
 namespace Pento.Infrastructure.Authentication;
 
 internal sealed class JwtBearerConfigureOptions(IConfiguration configuration)
@@ -12,6 +13,22 @@ internal sealed class JwtBearerConfigureOptions(IConfiguration configuration)
     public void Configure(JwtBearerOptions options)
     {
         configuration.GetSection(ConfigurationSectionName).Bind(options);
+
+        options.Events ??= new JwtBearerEvents();
+
+        options.Events.OnMessageReceived = context =>
+        {
+            StringValues accessToken = context.Request.Query["access_token"];
+            PathString path = context.HttpContext.Request.Path;
+
+            if (!string.IsNullOrEmpty(accessToken)
+                && path.StartsWithSegments("/message-hub"))
+            {
+                context.Token = accessToken;
+            }
+
+            return Task.CompletedTask;
+        };
     }
 
     public void Configure(string? name, JwtBearerOptions options)
